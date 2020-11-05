80dab85567482f30ba3a19653654127ccf76ff48
Ubuntu <ubuntu@ip-172-31-23-23.us-east-2.compute.internal>
2020-11-05 08:08:14 +0000

    20201105

--- FinalProject/FinalProject/settings.py
+++ FinalProject/FinalProject/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = ')z2vgmsk81&=vc47su76lm5#d@d2jum0hnmb7s+k%rur^9afs0'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['*']
 
@@ -120,7 +120,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_DIR = os.path.join(BASE_DIR, 'static')
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'static'),
+        STATIC_DIR,
 ]
 STATIC_ROOT = os.path.join(BASE_DIR, 'static_files')
